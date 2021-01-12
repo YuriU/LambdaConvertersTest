a258627fd3dda4b085acbd5b958ca3b7f123603f
YuriU <jerryhire8@gmail.com>
2021-01-12 23:54:56 +0200

    Added publishing to fanout topic

--- 03_TestConvertersApp/Handler.cs
+++ 03_TestConvertersApp/Handler.cs
@@ -1,19 +1,44 @@
-using System.Threading;
+using System;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using System.Text.Json;
+using System.Threading;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
+        private static AmazonSimpleNotificationServiceClient _snsClient = new AmazonSimpleNotificationServiceClient();
+      
        public async Task Hello(S3Event @event, ILambdaContext context)
        {
+           var topicArn = Environment.GetEnvironmentVariable("FILE_UPLOADED_TOPIC_ARN");
            LambdaLogger.Log("Event handler started");
-           var serialized = JsonSerializer.Serialize(@event);
-           LambdaLogger.Log(serialized);
+           if (@event.Records != null)
+           {
+               foreach (var record in @event.Records)
+               {
+                   var fileUploadedEvent = new
+                   {
+                       BucketName = record.S3.Bucket,
+                       Key = record.S3.Object.Key
+                   };
+                   
+                   var publishRequest = new PublishRequest
+                   {
+                       Message = JsonSerializer.Serialize(fileUploadedEvent),
+                       TopicArn = topicArn
+                   };
+
+                   await _snsClient.PublishAsync(publishRequest, CancellationToken.None);
+               }
+           }
+           
            LambdaLogger.Log("Event handler completed");
        }
     }
