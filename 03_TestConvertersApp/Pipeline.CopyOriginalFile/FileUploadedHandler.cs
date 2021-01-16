d1489b48c846ef93be4c8392f7f90c62817d9215
YuriU <jerryhire8@gmail.com>
2021-01-17 01:29:05 +0200

    Fixed permissions

--- 03_TestConvertersApp/Pipeline.CopyOriginalFile/FileUploadedHandler.cs
+++ 03_TestConvertersApp/Pipeline.CopyOriginalFile/FileUploadedHandler.cs
@@ -22,29 +22,38 @@ namespace Pipeline.CopyOriginalFile
             {
                 foreach (var record in @event.Records)
                 {
-                    try
-                    {
-                        var file = JsonSerializer.Deserialize<FileUploadedEvent>(record.Body);
-                        await ProcessEvent(file);
-                    }
-                    catch (Exception e)
-                    {
-                        LambdaLogger.Log("Error during publish message");
-                        LambdaLogger.Log(e.ToString());
-                        throw;
-                    }
+                    var file = JsonSerializer.Deserialize<FileUploadedEvent>(record.Body);
+                    await ProcessEvent(file);
                 }
             }
+        }
+
+        public async Task ProcessOriginalFileUploaded(FileUploadedEvent @event, ILambdaContext context)
+        {
+            context.Logger.Log($"Processing event {JsonSerializer.Serialize(@event)}");
+            await ProcessEvent(@event);
         }
 
         private async Task ProcessEvent(FileUploadedEvent file)
         {
             var tempFilePath = GetTempFilePath(file.Key);
-            var request = new GetObjectRequest { BucketName = file.OriginalBucketName, Key = file.Key };
+            await DownloadFile(file.OriginalBucketName, file.Key, tempFilePath);
+            await UploadFile(tempFilePath, file.ResultBucketName, file.Key);
+        }
+
+        private static async Task DownloadFile(string srcBucket, string srcKey, string destFileName)
+        {
+            var request = new GetObjectRequest { BucketName = srcBucket, Key = srcKey };
             using (GetObjectResponse response = await S3Client.GetObjectAsync(request))
             {
-                await response.WriteResponseStreamToFileAsync(tempFilePath, false, CancellationToken.None);
+                await response.WriteResponseStreamToFileAsync(destFileName, false, CancellationToken.None);
             }
+        }
+        
+        private static async Task UploadFile(string srcFileName, string destBucket, string destFileName)
+        {
+            var request = new PutObjectRequest { BucketName = destBucket, Key = destFileName, FilePath = srcFileName };
+            var response = await S3Client.PutObjectAsync(request, CancellationToken.None);
         }
 
         private string GetTempFilePath(string key)
