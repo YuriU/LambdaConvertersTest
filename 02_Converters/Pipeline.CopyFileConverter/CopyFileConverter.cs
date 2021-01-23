15cb0a96cb9e6db4321796e83da532a2c0f46ca1
YuriU <jerryhire8@gmail.com>
2021-01-23 22:22:56 +0200

    Fixed converter

--- 02_Converters/Pipeline.CopyFileConverter/CopyFileConverter.cs
+++ 02_Converters/Pipeline.CopyFileConverter/CopyFileConverter.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Amazon.Lambda.Core;
 using Pipeline.BaseConverterLambda;
 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace Pipeline.CopyFileConverter
 {
     public class CopyFileConverter : BaseConverterHandler
     {
         public override Task<string> Convert(string originalFile)
         {
+            var tempPath = Path.GetTempPath();
             var newFileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(originalFile)}";
-            File.Copy(originalFile, newFileName);
-            return Task.FromResult(newFileName);
+            var newPath = Path.Combine(tempPath, newFileName);
+            File.Copy(originalFile, newPath);
+            return Task.FromResult(newPath);
         }
     }
 }
