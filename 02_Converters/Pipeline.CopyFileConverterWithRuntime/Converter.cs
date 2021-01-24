462d614d61bffbb03319d983bbc41c62c8f38b32
YuriU <jerryhire8@gmail.com>
2021-01-24 17:29:15 +0200

    Added copy converter with built in bootstrapper based on debian base image

---
+++ 02_Converters/Pipeline.CopyFileConverterWithRuntime/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.SQSEvents;
+using Pipeline.BaseConverterLambda;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace Pipeline.CopyFileConverterWithRuntime
+{
+    public class Converter : BaseConverterHandler
+    {
+        public static async Task Main(string[] args)
+        {
+            var converter = new Converter();
+            Func<SQSEvent, ILambdaContext, Task> func = converter.Process;
+            using(var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer()))
+            using(var bootstrap = new LambdaBootstrap(handlerWrapper))
+            {
+                await bootstrap.RunAsync();
+            }
+        }
+        public override Task<string> Convert(string originalFile)
+        {
+            var tempPath = Path.GetTempPath();
+            var newFileName = $"{Guid.NewGuid().ToString()}{Path.GetExtension(originalFile)}";
+            var newPath = Path.Combine(tempPath, newFileName);
+            File.Copy(originalFile, newPath);
+            return Task.FromResult(newPath);
+        }
+    }
+}
