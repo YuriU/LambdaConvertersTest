835ab889a4382bf297f459792f18f60c194997a2
YuriU <jerryhire8@gmail.com>
2021-02-03 23:27:05 +0200

    Rewriten GetUploadUrl lambda to NodeJs

--- 03_TestConvertersApp/Backend_node/api_handler.js
+++ 03_TestConvertersApp/Backend_node/api_handler.js
@@ -1,17 +1,44 @@
 'use strict'
 
-var JobsTable = require('./lib/JobsTable')
-const jobsTable = new JobsTable(process.env.CONVERSION_JOBS_TABLE_NAME)
+
+const JobsTable = require('./lib/JobsTable')
+const AWS = require('aws-sdk')
+AWS.config.setPromisesDependency(Promise)
 
 module.exports.getJobs = async (event, context) => {
+    const jobsTable = new JobsTable(process.env.CONVERSION_JOBS_TABLE_NAME)
     const jobs = await jobsTable.getJobs()
+    return generateResponse(200, jobs);
+};
+
+module.exports.getUploadUrl = async (event, context) => {
+       
+    const s3 = new AWS.S3({signatureVersion: 'v4'});
+    console.log(event);
+    const fileName = event.queryStringParameters.filename;
     
+    const uploadKey = uuidv4() + '/' + fileName;
+
+    var params = { Bucket: process.env.UPLOAD_ORIGINAL_BUCKET_NAME, Key: uploadKey, Expires: 60 };
+    var url = s3.getSignedUrl('putObject', params);
+
+    return generateResponse(200, ({ Url: url }) );
+}
+
+const generateResponse = (status, message) => {
     return {
-        statusCode: 200,
-        headers: { 
-            'Content-Type' : 'application/json',
-            'Access-Control-Allow-Origin': '*' 
+      statusCode: status,
+      headers: { 
+          'Access-Control-Allow-Origin': '*', 
+          'Content-Type' : 'application/json',
         },
-        body: JSON.stringify(jobs)
-      }
+      body : JSON.stringify(message)
+    }
+};
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   }
